fc3631df5d1595e49d40f3596b9155b8f77ee666
unknown <alvito_rifky_29rpl@student.smktelkom-mlg.sch.id>
2021-03-17 10:33:31 +0700

    Initial Commit

---
+++ src/oop1/kelulusan.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop1;
+
+/**
+ *
+ * @author MOKLET-1
+ */
+public class kelulusan {
+    /**
+ * @param args the command line arguments
+ */
+ public static void main(String[] args) {
+     // TODO code application logic here
+     //object
+    siswa kelulusan = new siswa();
+
+ //pengisian Atribut
+ String nama  = "Anya";
+     int nilai = 80;
+ //menjalankan method
+ kelulusan.siswa();
+
+//menentukan lulus
+ if(nilai >=70){
+ System.out.println("selamat anda lulus");
+ }
+ else if(nilai <70){
+ System.out.println("Anda harus belajar lebih giat lagi");
+ }}}
+
+    
+
+
+    
+
+   
+
